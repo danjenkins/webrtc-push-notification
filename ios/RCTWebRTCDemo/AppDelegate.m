751fb5a8be6825818e975ca4040de788e89ebfe1
Nam Giang <namgiang@Nams-MacBook-Pro.local>
2017-10-17 16:09:06 +0200

    adding callkit

--- ios/RCTWebRTCDemo/AppDelegate.m
+++ ios/RCTWebRTCDemo/AppDelegate.m
@@ -11,6 +11,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNCallKit.h"
 
 @implementation AppDelegate
 
@@ -20,10 +21,24 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"RCTWebRTCDemo"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
+  // RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+  //                                                     moduleName:@"RCTWebRTCDemo"
+  //                                              initialProperties:nil
+  //                                                  launchOptions:launchOptions];
+
+  // Initialise RNCallKit
+  RNCallKit *rncallkit = [[RNCallKit alloc] init];
+
+  // Initialise React Bridge with RNCallKit
+  RCTBridge *bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation
+                                           moduleProvider:^{ return @[rncallkit]; }
+                                            launchOptions:launchOptions];
+
+  // Initialise React Root View with React Bridge you've just created
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"RCTWebRTCDemo"
+                                            initialProperties:nil];
+
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
@@ -32,6 +47,15 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+continueUserActivity:(NSUserActivity *)userActivity
+  restorationHandler:(void(^)(NSArray * __nullable restorableObjects))restorationHandler
+{
+  return [RNCallKit application:application
+           continueUserActivity:userActivity
+             restorationHandler:restorationHandler];
 }
 
 @end
--- main.js
+++ main.js
@@ -26,6 +26,12 @@ import {
   getUserMedia,
 } from 'react-native-webrtc';
 
+import RNCallKit from 'react-native-callkit';
+
+import uuid from 'uuid';
+
+import { SIPml } from 'SIPml-api';
+
 const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
 
 const pcPeers = {};
@@ -213,7 +219,7 @@ socket.on('connect', function(data) {
   getLocalStream(true, function(stream) {
     localStream = stream;
     container.setState({selfViewSrc: stream.toURL()});
-    container.setState({status: 'ready', info: 'Please enter or create room ID'});
+    container.setState({status: 'ready', info: 'Please Enter or Create room ID'});
   });
 });
 
@@ -245,6 +251,23 @@ let container;
 
 const RCTWebRTCDemo = React.createClass({
   getInitialState: function() {
+    // Initialise RNCallKit
+    let options = {
+        appName: 'RNCallKitExample',
+        imageName: 'my_image_name_in_bundle',
+        ringtoneSound: 'my_ringtone_sound_filename_in_bundle',
+    };
+    try {
+        RNCallKit.setup(options);
+    } catch (err) {
+        console.log('error:', err.message);
+    }
+    let that = this;
+    // Add RNCallKit Events
+    RNCallKit.addEventListener('didReceiveStartCallAction', that.onRNCallKitDidReceiveStartCallAction);
+    RNCallKit.addEventListener('answerCall', that.onRNCallKitPerformAnswerCallAction);
+    RNCallKit.addEventListener('endCall', that.onRNCallKitPerformEndCallAction);
+    RNCallKit.addEventListener('didActivateAudioSession', that.onRNCallKitDidActivateAudioSession);
     this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => true});
     return {
       info: 'Initializing',
@@ -261,6 +284,66 @@ const RCTWebRTCDemo = React.createClass({
   componentDidMount: function() {
     container = this;
   },
+  onRNCallKitDidReceiveStartCallAction(data) {
+    /*
+     * Your normal start call action
+     *
+     * ...
+     *
+     */
+
+    let _uuid = uuid.v4();
+    RNCallKit.startCall(_uuid, data.handle);
+  },
+
+  onRNCallKitPerformAnswerCallAction(data) {
+    /* You will get this event when the user answer the incoming call
+     *
+     * Try to do your normal Answering actions here
+     *
+     * e.g. this.handleAnswerCall(data.callUUID);
+     */
+  },
+
+  onRNCallKitPerformEndCallAction(data) {
+    /* You will get this event when the user finish the incoming/outgoing call
+     *
+     * Try to do your normal Hang Up actions here
+     *
+     * e.g. this.handleHangUpCall(data.callUUID);
+     */
+  },
+
+  onRNCallKitDidActivateAudioSession(data) {
+    /* You will get this event when the the AudioSession has been activated by **RNCallKit**,
+     * you might want to do following things when receiving this event:
+     *
+     * - Start playing ringback if it is an outgoing call
+     */
+  },
+
+  // This is a fake function where you can receive incoming call notifications
+  onIncomingCall() {
+    // Store the generated uuid somewhere
+    // You will need this when calling RNCallKit.endCall()
+    let _uuid = uuid.v4();
+    RNCallKit.displayIncomingCall(_uuid, "886900000000")
+  },
+
+  // This is a fake function where you make outgoing calls
+  onOutgoingCall() {
+    // Store the generated uuid somewhere
+    // You will need this when calling RNCallKit.endCall()
+    let _uuid = uuid.v4();
+    RNCallKit.startCall(_uuid, "886900000000")
+  },
+
+  // This is a fake function where you hang up calls
+  onHangUpCall() {
+    // get the _uuid you stored earlier
+    RNCallKit.endCall(_uuid)
+  },
+
   _press(event) {
     this.refs.roomID.blur();
     this.setState({status: 'connect', info: 'Connecting'});
